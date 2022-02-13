b6ed4608598f15f1eea75cc598daf75b098c80ae
Usaku Inaoka <99005797+sasuke0601@users.noreply.github.com>
2022-02-14 01:21:09 +0900

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="main-content" style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
       <h1>Hi&nbsp;👋</h1>
-      <p>This is solana wallet connect bolierplate. Injoy now!</p>
+      <p>This is the Solana wallet connect boilerplate. Enjoy now!</p>
     </div>
   );
 }
